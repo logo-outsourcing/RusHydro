c8bdde69a50738b7becb20295221f4e1c5915c9b
= <vikytrusowa.@gmail.com>
2020-05-04 14:36:42 +0300

    add animate

--- js/main.js
+++ js/main.js
@@ -74,8 +74,6 @@ function getResults() {
 }
 
 $(document).ready(function () {
-
-
     setTimeout(function () {
         $("#preloader").addClass('preload__animate');
         setTimeout(function () {
@@ -89,6 +87,7 @@ $(document).ready(function () {
         }, 2900);
 
     });
+
 
     //header video
 
@@ -252,13 +251,12 @@ $(document).ready(function () {
         setTimeout(function () {
             $(modal).parents(".overlay").removeClass("open");
         }, 350);
-
     });
 
 
     $(".interactive__farther").click(function () {
         var question = $(this).attr('data-src');
-        if (question < 13) {
+        if (question < 12) {
             var current = '#question_' + question;
             var next = '#question_' + (parseInt(question) + 1);
             $(current).addClass('interactive__item--hidden');
@@ -268,4 +266,109 @@ $(document).ready(function () {
             }, 100);
         }
     });
+    $('.button').click(function () {
+        setTimeout(function () {
+            $('#question_12').addClass('interactive__item--hidden');
+            $('#question_13').removeClass('interactive__item--hidden');
+            setTimeout(function () {
+                $('.interactive__box').addClass('interactive__box--open');
+            },500);
+
+        },900);
+    });
+
+
+    gsap.registerPlugin(Physics2DPlugin);
+
+    document.querySelectorAll('.button').forEach(button => {
+
+        const bounding = button.getBoundingClientRect()
+
+        button.addEventListener('mousemove', e => {
+
+            let dy = (e.clientY - bounding.top - bounding.height / 2) / -1
+            let dx = (e.clientX - bounding.left - bounding.width / 2) / 10
+
+            dy = dy > 10 ? 10 : (dy < -10 ? -10 : dy);
+            dx = dx > 4 ? 4 : (dx < -4 ? -4 : dx);
+
+            button.style.setProperty('--rx', dy);
+            button.style.setProperty('--ry', dx);
+
+        });
+
+        button.addEventListener('mouseleave', e => {
+
+            button.style.setProperty('--rx', 0)
+            button.style.setProperty('--ry', 0)
+
+        });
+
+        button.addEventListener('click', e => {
+            button.classList.add('success');
+            gsap.to(button, {
+                '--icon-x': -3,
+                '--icon-y': 3,
+                '--z-before': 0,
+                duration: .2,
+                onComplete() {
+                    particles(button.querySelector('.emitter'), 100, -4, 6, -80, -50);
+                    gsap.to(button, {
+                        '--icon-x': 0,
+                        '--icon-y': 0,
+                        '--z-before': -6,
+                        duration: 1,
+                        ease: 'elastic.out(1, .5)',
+                        onComplete() {
+                            button.classList.remove('success');
+                        }
+                    });
+                }
+            });
+        });
+
+    });
+
+    function particles(parent, quantity, x, y, minAngle, maxAngle) {
+        let colors = [
+            '#FFFF04',
+            '#EA4C89',
+            '#892AB8',
+            '#4AF2FD',
+        ];
+        for (let i = quantity - 1; i >= 0; i--) {
+            let angle = gsap.utils.random(minAngle, maxAngle),
+                velocity = gsap.utils.random(70, 140),
+                dot = document.createElement('div');
+            dot.style.setProperty('--b', colors[Math.floor(gsap.utils.random(0, 4))]);
+            parent.appendChild(dot);
+            gsap.set(dot, {
+                opacity: 0,
+                x: x,
+                y: y,
+                scale: gsap.utils.random(.4, .7)
+            });
+            gsap.timeline({
+                onComplete() {
+                    dot.remove();
+                }
+            }).to(dot, {
+                duration: .05,
+                opacity: 1
+            }, 0).to(dot, {
+                duration: 1.8,
+                rotationX: `-=${gsap.utils.random(720, 1440)}`,
+                rotationZ: `+=${gsap.utils.random(720, 1440)}`,
+                physics2D: {
+                    angle: angle,
+                    velocity: velocity,
+                    gravity: 120
+                }
+            }, 0).to(dot, {
+                duration: 1,
+                opacity: 0
+            }, .8);
+        }
+    }
+
 });
